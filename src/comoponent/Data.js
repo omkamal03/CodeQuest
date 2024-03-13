4b6d6d5f75130bf198cadb8f849bf2ba41f13622
Om <omkamalpce123@gmail.com>
2024-03-14 00:15:45 +0530

    Add data file

---
+++ src/comoponent/Data.js
@@ -0,0 +1,47 @@
+const all_students=[
+    {
+        name:"Om kamal",
+        regNo:"22105131001",
+        rollNo:"22505",
+        branch:"C.S.E",
+        year:"2022-26",
+        field:"Hackathon"
+
+    },
+    {
+        name:"Prince kr Gupta",
+        regNo:"22105131017",
+        rollNo:"22506",
+        branch:"C.S.E",
+        year:"2022-26",
+        field:"Hackathon"
+
+    },
+    {
+        name:"Reshav Raj",
+        regNo:"22105131014",
+        rollNo:"22523",
+        branch:"C.S.E",
+        year:"2022-26",
+        field:"Hackathon"
+
+    },
+    {
+        name:"Raushan kumar",
+        regNo:"22105131011",
+        rollNo:"22511",
+        branch:"C.S.E(AI)",
+        year:"2022-26",
+        field:"Hackathon"
+
+    },
+    {
+        name:"Vishwajit singh",
+        regNo:"22105131021",
+        rollNo:"22501",
+        branch:"C.S.E",
+        year:"2022-26",
+        field:"Hackathon"
+
+    }
+]
